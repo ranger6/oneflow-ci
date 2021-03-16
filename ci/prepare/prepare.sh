73f803390ed4cbcc5726709f309893573c2a5f64
ranger6 <ranger6@users.noreply.github.com>
2021-03-16 15:13:35 +0100

    move tagging in prepare to shell script

--- ci/prepare/prepare.sh
+++ ci/prepare/prepare.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-#	prepare -- prepare a pre-release
-#                  $1 is tag annotation
+#       prepare -- tag the HEAD of the release branch with
+#                  pre-release version, merge back in the put step
 
 set -ex
 
-(echo -n "tagging " ; cat "$1") > prerel/tag_annotation
+target_version="$(cat $1)"
+release_branch="$2"
+
+cd release
+
+git checkout "$release_branch"
+
+git config --global user.email "ranger6@users.noreply.github.com"
+git config --global user.name "ranger6"
+
+git tag -a -m "tagging pre-release at $target_version" "$target_version"
