e013967d5c7288b1ad9951415c0b1560949b2aac
ranger6 <ranger6@users.noreply.github.com>
2021-03-26 11:37:06 +0100

    Clean does not work, remove/disable tentatively.
    Seems a lack of capabilty with concourse.

--- ci/clean/clean.sh
+++ ci/clean/clean.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 #	clean -- clean up after release, delete release branch
+#		 This code does not work: comment out for now.
 
 set -ex
 
 target_version="$(cat $1)"
 release_branch="$2"
 
-git clone release release-out
-cd release-out
-
-git checkout $target_version
-
-git config --global user.email "ranger6@users.noreply.github.com"
-git config --global user.name "ranger6"
-
-git branch -D "$release_branch"
+# git clone release release-out
+# cd release-out
+# 
+# git checkout $target_version
+# 
+# git config --global user.email "ranger6@users.noreply.github.com"
+# git config --global user.name "ranger6"
+# 
+# git branch -D "$release_branch"
