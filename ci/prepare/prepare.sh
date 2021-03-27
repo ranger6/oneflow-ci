f2401ab3ca9b609c18f4573c3793bf7cca1bb68e
ranger6 <ranger6@users.noreply.github.com>
2021-03-27 10:23:49 +0100

    remove clone from prepare

--- ci/prepare/prepare.sh
+++ ci/prepare/prepare.sh
@@ -8,12 +8,11 @@ set -ex
 target_version="$(cat $1)"
 release_branch="$2"
 
-git clone release release-out
-cd release-out
-
-git checkout "$release_branch"
+cd release
 
 git config --global user.email "ranger6@users.noreply.github.com"
 git config --global user.name "ranger6"
+
+git checkout "$release_branch"
 
 git tag -a -m "tagging pre-release at $target_version" "$target_version"
