166e338fc4984581e6626f9a801c3beff3ebc7e7
ranger6 <ranger6@users.noreply.github.com>
2021-03-27 09:59:15 +0100

    remove clone in start.sh, just use the main copy
    Note that "main" is "put" to a different resource from main.

--- ci/start/start.sh
+++ ci/start/start.sh
@@ -13,12 +13,11 @@ release_branch="${release_branch_prefix}${target_version}"
 
 echo "{\"release-branch-name\": \"${release_branch}\"}" > release-vars/vars.json
 
-git clone main release
-cd release
-git checkout -b "$release_branch" "$branch_hash"
-
 git config --global user.email "ranger6@users.noreply.github.com"
 git config --global user.name "ranger6"
+
+cd main
+git checkout -b "$release_branch" "$branch_hash"
 
 echo $target_version > version.txt
 
