779df027b90bae100c6d85ed68fd49d82e87d3ab
ranger6 <ranger6@users.noreply.github.com>
2021-03-27 10:32:12 +0100

    minor edit

--- ci/update/update.sh
+++ ci/update/update.sh
@@ -10,10 +10,10 @@ stable_branch="$3"
 
 cd stable
 
-git checkout $stable_branch
-git remote add -f --tags -t "$release_branch" release ../release
-
 git config --global user.email "ranger6@users.noreply.github.com"
 git config --global user.name "ranger6"
+
+git checkout $stable_branch
+git remote add -f --tags -t "$release_branch" release ../release
 
 git merge --ff-only "$target_version" 
