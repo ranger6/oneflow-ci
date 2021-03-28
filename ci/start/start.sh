a34262a5745549fecb0894a55bb4bf7ab9941d6c
ranger6 <ranger6@users.noreply.github.com>
2021-03-28 18:22:07 +0200

    don't do semver bumps twice

--- ci/start/start.sh
+++ ci/start/start.sh
@@ -13,13 +13,17 @@ release_branch="${release_branch_prefix}${target_version}"
 
 echo "{\"release-branch-name\": \"${release_branch}\"}" > release-vars/vars.json
 
+cd main
+
 git config --global user.email "ranger6@users.noreply.github.com"
 git config --global user.name "ranger6"
 
-cd main
 git checkout -b "$release_branch" "$branch_hash"
+
+#	this is a placeholder action for what might need
+#	to be done when preparing a new release.
 
 echo $target_version > version.txt
 
 git add .
-git commit -m "set target version $target_version on $release_branch"
+git commit -m "starting new release branch \"$release_branch\" for target version \"$target_version\""
