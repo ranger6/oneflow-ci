df012c40a67a170028cded79bef5d2d7790b34d3
ranger6 <ranger6@users.noreply.github.com>
2021-03-31 10:54:47 +0200

    fix silly error

--- ci/release/release.sh
+++ ci/release/release.sh
@@ -9,17 +9,15 @@ target_version="$(cat $1)"
 release_branch="$2"
 main_branch="$3"
 
-cd release
-
-git config --global user.email "ranger6@users.noreply.github.com"
-git config --global user.name "ranger6"
-
 # git checkout "$release_branch"
 # git tag -a -m "tagging release at $target_version" "$target_version"
 echo "$target_version" > tags/name
 echo "tagging release at $target_version" > tags/annotation
 
 cd ../main
+
+git config --global user.email "ranger6@users.noreply.github.com"
+git config --global user.name "ranger6"
 
 git checkout $main_branch
 git pull --no-edit --no-rebase ../release "$release_branch"
